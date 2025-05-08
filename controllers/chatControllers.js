a81ab9421ed65ee81b5ef986c73749ee41c77f80
Saif <ysaifnijat@gmail.com>
2025-05-08 10:23:50 -0400

    nothing special

--- controllers/chatControllers.js
+++ controllers/chatControllers.js
@@ -1,57 +1,6 @@
 const OpenAI = require("openai");
 const axios = require('axios');
 
-// ✅ Replace with your actual Gemini API key
-// const openai = new OpenAI({
-//   apiKey: "AIzaSyA7ACDCE8nikoRfRfxVoV67Q6Xxn5jN7oQ",
-//   baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
-// // });
-
-// const chatWithAILocal = async (req, res) => {
-//   const { message } = req.body;
-
-//   if (!message) {
-//     return res.status(400).json({ reply: 'Message is required.' });
-//   }
-
-//   try {
-//     const response = await openai.chat.completions.create({
-//       model: "gemini-1.5-flash",
-//       messages: [
-//         {
-//           role: "system",
-//           content: "You are a helpful assistant."
-//         },
-//         {
-//           role: "user",
-//           content: message
-//         }
-//       ]
-//     });
-
-//     const reply = response.choices[0]?.message?.content || "No reply from Gemini.";
-//     res.json({ reply });
-//   } catch (error) {
-//     console.error("Gemini (OpenAI wrapper) error:", error.message);
-//     res.status(500).json({ reply: "Error communicating with Gemini (OpenAI style)." });
-//   }
-// };
-
-
-/*
-
-curl "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions" \
--H "Content-Type: application/json" \
--H "Authorization: Bearer GEMINI_API_KEY" \
--d '{
-    "model": "gemini-2.0-flash",
-    "messages": [
-        {"role": "user", "content": "Explain to me how AI works"}
-    ]
-    }'
-
-*/
-
 const chatWithAI = async (req, res) => {
   const { message } = req.body;
 
@@ -63,7 +12,6 @@ const chatWithAI = async (req, res) => {
   if (!message) {
     return res.status(400).json({ reply: 'Message is required.' });
   }
-
   try {
     const response = await axios.post('https://generativelanguage.googleapis.com/v1beta/openai/chat/completions', {
       model: 'gemini-2.0-flash', 
